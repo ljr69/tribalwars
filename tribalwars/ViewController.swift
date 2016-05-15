65cef3ee16126c480e218b86cc8fbcc4d8e6fbe2
lroseblade <sparcuk@gmail.com>
2016-05-15 19:59:56 +0100

    Screen elements complete. Outlets and Actions added

--- tribalwars/ViewController.swift
+++ tribalwars/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var player1Img: UIImageView!
+    @IBOutlet weak var player2Img: UIImageView!
+    @IBOutlet weak var printMsgLbl: UILabel!
+    @IBOutlet weak var p1AttackLbl: UILabel!
+    @IBOutlet weak var p2AttackLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func p1AttackPressed(sender: AnyObject) {
+    }
 
+    @IBAction func p2AttackPressed(sender: AnyObject) {
+    }
+    
 }
 
